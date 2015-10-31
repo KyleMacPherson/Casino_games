52a442f3315bdc4d4dc8d17cd9d2f03c712dbdfb
kyle macpherson <kaimac2010@gmail.com>
2015-10-31 20:51:14 +0000

    sessions model and controller added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+  def create
+    hash = JSON.parse(request.body.read)
+    p hash
+    user = User.find_by_email(hash["email"])
+    if user && user.authenticate(hash["password"])
+      access_token = SecureRandom.hex
+      user.access_token = access_token
+      render json: user
+    end
+  end
+
+  def create
+    hash = JSON.parse(request.body.read)
+    p hash
+    user = User.find_by_email(hash["email"])
+    if user && user.authenticate(hash["password"])
+      user.destroy     
+    end
+  end
+
 end
